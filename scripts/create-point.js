f12d4cf331fb925134c6537a439dd3a434107d34
Frederico Damian Preve Neto <fredpreve@gmail.com>
2020-06-04 21:47:46 -0300

    Create search results and create point screens

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -26,6 +26,8 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = ""
+    citySelect.disabled = true
 
     fetch(url)
     .then((res) => { return res.json() })
@@ -34,7 +36,7 @@ function getCities(event) {
             citySelect.innerHTML += `<option value ="${city.id}">${city.nome}</option>`
         }
         
-        citySelect.enabled = true
+        citySelect.disabled = false
     } )
 
 
@@ -43,3 +45,48 @@ function getCities(event) {
 document
 .querySelector("select[name=uf]")
 .addEventListener("change", getCities)
+
+
+// itens de coleta
+
+// catch all li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+// 
+
+// update hidden field with selected items
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    
+    // add or remove class with js
+    itemLi.classList.toggle("selected")
+
+    const itemId = event.target.dataset.id;
+
+    // check if there are selected items.
+    const alreadySelected = selectedItems.findIndex( (item) => { 
+        return item == itemId
+    })
+    
+    // If current item is already selected, then unselected it
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+         })
+
+         selectedItems = filteredItems
+    } else {
+        // if don't, add the selected item to the selected items array
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+}
